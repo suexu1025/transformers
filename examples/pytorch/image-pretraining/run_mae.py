4c36ed82f321fedc4ba3eaf87bde532eab1bfb96
Qinwen Xu <suexu1025@gmail.com>
2023-02-15 19:47:07 +0000

    use xmp_spawn call

--- examples/pytorch/image-pretraining/run_mae.py
+++ examples/pytorch/image-pretraining/run_mae.py
@@ -166,7 +166,10 @@ def main():
     # See all possible arguments in src/transformers/training_args.py
     # or by passing the --help flag to this script.
     # We now keep distinct sets of args, for a cleaner separation of concerns.
-
+    import os
+    import pprint
+    pprint(os.environ)
+    
     parser = HfArgumentParser((ModelArguments, DataTrainingArguments, CustomTrainingArguments))
     if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
         # If we pass only one argument to the script and it's the path to a json file,
@@ -393,4 +396,5 @@ def _mp_fn(index):
 
 
 if __name__ == "__main__":
-    main()
+    xmp.spawn(main, args=(), nprocs=4)
+    #main()
